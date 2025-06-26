984fd92aba748da49a5402cd8912b6c681802d9d
rebpacquiao <reboden.pacquiao@idp.com>
2025-06-26 16:36:11 +0800

    feat: Add Comming Soon page and update sidebar navigation

---
+++ src/app/admin/comming-soon/page.tsx
@@ -0,0 +1,12 @@
+export default function CommingSoonPage() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
+            <h1 className="text-3xl font-bold text-gray-800 dark:text-gray-200 mb-4">
+                Comming Soon!
+            </h1>
+            <p className="text-gray-600 dark:text-gray-400">
+                This page is under construction. Stay tuned for updates!
+            </p>
+        </div>
+    );
+}
--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -55,26 +55,27 @@ const navItems: NavItem[] = [
   {
     icon: <CareerLadderIcon />,
     name: "Careers",
-    path: "/admin/careers",
+    path: "/admin/careers", // /admin/careers
      //subItems: [{ name: "Form Elements", path: "/form-elements", pro: false }],
   },
   
   {
     icon: <TicketIcon />,
     name: "Tickets",
-    path: "/admin/tickets",
+    path: "/admin/comming-soon", // /admin/tickets
      //subItems: [{ name: "Form Elements", path: "/form-elements", pro: false }],
   },
-  {
-    icon: <BlogIcon />,
-    name: "Blog",
-    path: "/admin/blog",
-     subItems: [{ name: "All Posts", path: "/admin/blog", pro: false },
-      { name: "Add New", path: "/admin/blog/add-new", pro: false },
-      { name: "Categories", path: "/admin/blog/categories", pro: false },
-      { name: "Tags", path: "/admin/blog/tags", pro: false }
-     ],
-  },
+
+  // {
+  //   icon: <BlogIcon />,
+  //   name: "Blog",
+  //   path: "/admin/blog",
+  //    subItems: [{ name: "All Posts", path: "/admin/blog", pro: false },
+  //     { name: "Add New", path: "/admin/blog/add-new", pro: false },
+  //     { name: "Categories", path: "/admin/blog/categories", pro: false },
+  //     { name: "Tags", path: "/admin/blog/tags", pro: false }
+  //    ],
+  // },
 
 ];
 
