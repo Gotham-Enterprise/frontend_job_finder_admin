db7b8657d0860d90e4a2082667a656a7fda2e9a3
radenayr <radenayr@outlook.com>
2025-08-05 23:28:40 +0800

    JF-729 initiated UI for Admin ID Verification

---
+++ src/app/admin/id-approval/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import IDApproval from '@/components/page/IDApproval';
+
+const IDApprovalPage = () => {
+  return (
+    <IDApproval />
+  )
+}
+
+export default IDApprovalPage
