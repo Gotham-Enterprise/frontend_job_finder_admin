7fe15ce5b18a7773f41e2f7576b8c19c0d3bffb2
Marvin Jay Lagang <marvin@gothamenterprisesltd.com>
2025-09-15 17:14:11 +0800

    Fixes/marvin 09 13 2025 (#30)
    * feat: Add Deactivated Job Seekers management component
    
    - Implement DeactivatedJobSeekers component for viewing and managing deactivated job seekers.
    - Create DeactivationHistoryModal for displaying deactivation history of users.
    - Add ReactivateUserModal for reactivating deactivated users with a reason.
    - Update JobSeekersHeader to include a link to the Deactivated Users page.
    - Create adminUserManagement API service for handling user deactivation and reactivation.
    - Add necessary types and interfaces for API responses and requests.
    - Implement pagination and filtering for deactivated job seekers.
    
    * refactor: Update DeactivatedJobSeekers and DeactivationHistoryModal to use new data structure and improve clarity
    
    * feat: Add react-icons dependency and integrate icons in JobSeekersHeader and ProfileCard components
    
    * feat: Add BackToListButton to Deactivated Job Seekers page for improved navigation

--- src/components/page/JobSeekers/components/index.ts
+++ src/components/page/JobSeekers/components/index.ts
@@ -1,5 +1,9 @@
-export { default as JobSeekersHeader } from './JobSeekersHeader';
-export { default as JobSeekersFilters } from './JobSeekersFilters';
-export { default as JobSeekersTable } from './JobSeekersTable';
-export { default as JobSeekersTablePagination } from './JobSeekersTablePagination';
-export { EditJobSeekerModal } from './EditJobSeekerModal';
+export { default as JobSeekersHeader } from "./JobSeekersHeader";
+export { default as JobSeekersFilters } from "./JobSeekersFilters";
+export { default as JobSeekersTable } from "./JobSeekersTable";
+export { default as JobSeekersTablePagination } from "./JobSeekersTablePagination";
+export { EditJobSeekerModal } from "./EditJobSeekerModal";
+export { DeactivateUserModal } from "./DeactivateUserModal";
+export { ReactivateUserModal } from "./ReactivateUserModal";
+export { DeactivationHistoryModal } from "./DeactivationHistoryModal";
+export { DeactivatedJobSeekers } from "./DeactivatedJobSeekers";
