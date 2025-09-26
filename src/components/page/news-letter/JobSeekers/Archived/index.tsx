502dcf4e8da35c3a27651728fc83c2dda41bbdd7
rebpacquiao <reb.pacquiao@gmail.com>
2025-09-26 12:29:23 +0800

    Feature/add job seeker newsletters management components

---
+++ src/components/page/news-letter/JobSeekers/Archived/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function ArchivedNewsletters() {
+  return (
+    <div>
+      <h2 className="text-xl font-semibold mb-4">Archived Newsletters</h2>
+      <p>Here you can view and manage archived newsletters for job seekers.</p>
+      {/* Add archived newsletters content here */}
+    </div>
+  );
+}
