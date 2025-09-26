3200e8f3b4f1faf806f7b17d57953e2ac761bc7f
rebpacquiao <reb.pacquiao@gmail.com>
2025-09-26 12:45:43 +0800

    Feature/update newsletter component to include email subscription form

--- src/components/page/news-letter/index.tsx
+++ src/components/page/news-letter/index.tsx
@@ -3,11 +3,7 @@ import React from "react";
 const NewsLetterComponent = () => {
   return (
     <div>
-      <h1>Subscribe to our Newsletter</h1>
-      <form>
-        <input type="email" placeholder="Enter your email" />
-        <button type="submit">Subscribe</button>
-      </form>
+      <h1>Newsletter</h1>
     </div>
   );
 };
