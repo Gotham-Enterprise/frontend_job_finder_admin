4c2131c80d612cbca8cfd4159b01b99fa34cdd62
rebpacquiao <reb.pacquiao@gmail.com>
2025-10-03 15:17:29 +0800

    refactor: Remove unused state and context from EditStep component

--- src/components/page/news-letter/steps/EditStep.tsx
+++ src/components/page/news-letter/steps/EditStep.tsx
@@ -1,13 +1,9 @@
-import React, { useState } from "react";
-import { useNewsletter } from "../NewsletterContext";
+import React from "react";
 import ReactEmailEditor from "../components/ReactEmailEditor";
 
 const EditStep: React.FC = () => {
-  const { state } = useNewsletter();
-
   return (
     <div className="h-screen w-screen fixed inset-0 z-50 bg-white" style={{ height: "100vh", width: "100vw" }}>
-      {/* Main Editor */}
       <div className="h-full w-full" style={{ height: "100%", width: "100%" }}>
         <ReactEmailEditor />
       </div>
